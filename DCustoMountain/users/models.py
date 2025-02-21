55e062b8f9ce7606be6a9b15f46ef1167191e985
Won Eugene <luveugene1@gmail.com>
2025-02-21 12:28:59 +0900

    [ AbstractUser 생성 ] users/models.py 에 AbstractUser import users/models.py 에 User 생성 users/admin.py 에 User, UserAdmin import users/admin.py 에 User register DCM/settings.py 에 AUTH_USER_MODEL 추가 GitHub 낙서장 폴더 추가 GitHub 낙서장/TroubleShooting_원유진.txt 추가

--- DCustoMountain/users/models.py
+++ DCustoMountain/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
 # Create your models here.
+class User(AbstractUser): 
+    profile_image = models.ImageField(upload_to="users/profile", blank=True)
+    short_description = models.TextField(blank=True) 
+
+    def __str__(self): 
+        return self.username 
