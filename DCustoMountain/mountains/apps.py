e7c138def61e9350c111f2088fe95ef968318e08
EugeneWon <krisdrig@gmail.com>
2025-02-24 15:23:36 +0900

    mountains 앱 커밋

--- DCustoMountain/mountains/apps.py
+++ DCustoMountain/mountains/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MountainsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "mountains"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mountains'
--- DCustoMountain/mountains/models.py
+++ DCustoMountain/mountains/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Mountain(models.Model):
+    mountain = models.CharField(max_length=50) 
+    location = models.CharField(max_length=100) 
+    height = models.FloatField() 
+
+    def __str__(self):
+        return self.mountain 
+    
