d249650ae94e4f1c0b8d9cadacc3aeaeb1b2815e
SooyoungJung <tr248267@gmail.com>
2025-02-24 23:32:42 +0900

    크롤링2

--- DCustoMountain/drivers/crawlingMtnInfoDriver.py
+++ DCustoMountain/drivers/crawlingMtnInfoDriver.py
@@ -2,15 +2,6 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class getMtnName():
-    # 산 이름 가져오기
-    def mtnName(driver, mtnNameList):
-        mtn_names = driver.find_elements(By.CSS_SELECTOR, ".list_info strong")
-        for mtn_name in mtn_names:
-            mtnNameList.append(mtn_name.text)
-        
-        return mtnNameList
-
 class getRunChromeDriver():
     def runChromeDriver(url):
         # 크롬 드라이버 실행
@@ -26,18 +17,30 @@ class getRunChromeDriver():
         # 10초 안에 웹페이지를 load하면 바로 넘어가거나, 10초를 기다림
         driver.implicitly_wait(10)
         return driver
-    
 
-class mainDriver():
+
+class getMtnName():
+    # 산 이름 가져오기
+    def mtnName(driver, mtnNameList):
+        mtn_names = driver.find_elements(By.CSS_SELECTOR, ".list_info strong")
+        for mtn_name in mtn_names:
+            mtnNameList.append(mtn_name.text)
+        
+        return mtnNameList
+
+class getMtnNameMainDriver():
     url1 = "https://www.forest.go.kr/kfsweb/kfi/kfs/foreston/main/contents/FmmntSrch/selectFmmntSrchList.do?mntIndex="
-    urlnum = 1
+    urlpagenum = 1
     urltail = "&searchMnt=&searchCnd=&mn=AR02_02_05_01&orgId=&mntUnit=10"
     mtnNameList = []
 
-    for urlnum in range(1,11):
-        driver = getRunChromeDriver.runChromeDriver(url1+f'{urlnum}'+urltail)
+    for urlpagenum in range(1,11):
+        driver = getRunChromeDriver.runChromeDriver(url1+f'{urlpagenum}'+urltail)
         
         # 산 이름 가져오기
         mtnNameList = getMtnName.mtnName(driver, mtnNameList)
     
+    # 산 이름 리스트
     print(mtnNameList)
+
+
