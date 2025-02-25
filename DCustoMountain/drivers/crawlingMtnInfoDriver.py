8dc062bc4063beede1108ec737025cebbaa29971
Jung <tr248267@gmail.com>
2025-02-25 12:35:04 +0900

    크롤링오전수정분반영

--- DCustoMountain/drivers/crawlingMtnInfoDriver.py
+++ DCustoMountain/drivers/crawlingMtnInfoDriver.py
@@ -32,19 +32,20 @@ class getInfo():
         return mtnNameList
 
     # 지역 가져오기
-    def regionName(driver, regionNameList, mtn_order):
+    def locationName(driver, locationNameList, mtn_order):
         mtnTitle = driver.find_elements(By.CSS_SELECTOR, "ul.lst_thumb a")
         mtnTitle[mtn_order].click()
         time.sleep(1)
         soup = BeautifulSoup(driver.page_source, "lxml")
         result_name = soup.select("table.tbl td")
         # 결괏값이 있는 경우만 리스트에 추가, 없는 경우는 None을 추가
-        if result_name is not None or '':
-            regionNameList.append(result_name[1].string.strip())
+        result_value = result_name[1].string.strip()
+        if result_value != '':
+            locationNameList.append(result_value)
         else:
-            regionNameList.append(None)
+            locationNameList.append(None)
 
-        return regionNameList
+        return locationNameList
 
     # 산행기간 가져오기
     def periodMtnName(driver, periodList, mtn_order):
@@ -54,12 +55,45 @@ class getInfo():
         soup = BeautifulSoup(driver.page_source, "lxml")
         result_name = soup.select("table.tbl td")
         # 결괏값이 있는 경우만 리스트에 추가, 없는 경우는 None을 추가
-        if result_name is not None or '':
-            periodList.append(result_name[7].string.strip())
+        result_value = result_name[7].string.strip()
+        if result_name != '':
+            periodList.append(result_value)
         else:
             periodList.append(None)
 
         return periodList
+    
+    # 산 높이 가져오기
+    def mtnHeight(driver, mtnHeightList, mtn_order):
+        mtnTitle = driver.find_elements(By.CSS_SELECTOR, "ul.lst_thumb a")
+        mtnTitle[mtn_order].click()
+        time.sleep(1)
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        result_name = soup.select("table.tbl td")
+        # 결괏값이 있는 경우만 리스트에 추가, 없는 경우는 None을 추가
+        result_value = result_name[9].string.strip()
+        if result_name != '':
+            mtnHeightList.append(result_value)
+        else:
+            mtnHeightList.append(None)
+
+        return mtnHeightList
+    
+    # 산 난이도 가져오기
+    def mtnDifficulty(driver, mtnDifficultyList, mtn_order):
+        mtnTitle = driver.find_elements(By.CSS_SELECTOR, "ul.lst_thumb a")
+        mtnTitle[mtn_order].click()
+        time.sleep(1)
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        result_name = soup.select("table.tbl td")
+        # 결괏값이 있는 경우만 리스트에 추가, 없는 경우는 None을 추가
+        result_value = result_name[11].string.strip()
+        if result_name != '':
+            mtnDifficultyList.append(result_value)
+        else:
+            mtnDifficultyList.append(None)
+
+        return mtnDifficultyList
 
 
 
@@ -68,8 +102,10 @@ class getMainDriver():
     urlpagenum = 1
     urltail = "&searchMnt=&searchCnd=&mn=AR02_02_05_01&orgId=&mntUnit=10"
     mtnNameList = [] # 산 이름 리스트
-    regionNameList = [] # 지역 이름 리스트
+    locationNameList = [] # 지역 이름 리스트
     periodList = [] # 산행 기간 리스트
+    mtnHeightList = []
+    mtnDifficultyList = []
 
     # 산이름GET START
     for urlpagenum in range(1,11):
@@ -82,9 +118,9 @@ class getMainDriver():
     for urlpagenum in range(1,11):
         for mtn_order in range(0,10):
             driver = getRunChromeDriver.runChromeDriver(url1+f'{urlpagenum}'+urltail)
-            regionNameList = getInfo.regionName(driver, regionNameList, mtn_order)
+            locationNameList = getInfo.locationName(driver, locationNameList, mtn_order)
 
-    print(regionNameList)
+    print(locationNameList)
     # 지역이름GET END
 
     # 산행기간GET START
@@ -97,9 +133,22 @@ class getMainDriver():
     # 산행기간GET END
 
     # 산높이GET START
+    for urlpagenum in range(1,11):
+        for mtn_order in range(0,10):
+            driver = getRunChromeDriver.runChromeDriver(url1+f'{urlpagenum}'+urltail)
+            mtnHeightList = getInfo.periodMtnName(driver, mtnHeightList, mtn_order)
+
+    print(mtnHeightList)
     # 산높이GET END
     
+    # 난이도GET START
+    for urlpagenum in range(1,11):
+        for mtn_order in range(0,10):
+            driver = getRunChromeDriver.runChromeDriver(url1+f'{urlpagenum}'+urltail)
+            mtnDifficultyList = getInfo.mtnDifficulty(driver, mtnDifficultyList, mtn_order)
 
+    print(mtnDifficultyList)
+    # 난이도GET END
 
 
 
