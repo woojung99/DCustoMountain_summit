eb64a4e8caef75cbe9b50031326eb4714d2a2e62
chuuravel <tr248267@gmail.com>
2025-02-25 10:55:40 +0900

    Delete DCM_merge1 directory

--- DCM_merge1/cal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from cal import views 
-
-app_name = "cal" 
-urlpatterns = [
-    path("", views.CalendarView.as_view(), name="calendar"), 
-    # path("detail/<int:id>/", views.CalendarDetailView.as_view(), name="calendar_detail"), 
-]
--- DCM_merge1/mountains/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from mountains.models import Mountain 
-
-# Register your models here.
-@admin.register(Mountain) 
-class MountainAdmin(admin.ModelAdmin): 
-    pass 
