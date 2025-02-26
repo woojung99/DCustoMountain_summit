2c52e2a46a010013140806247187bb6d9b9caff8
SooyoungJung <tr248267@gmail.com>
2025-02-27 00:15:46 +0900

    Mounntain검색화면중간반영

--- DCustoMountain/mountains/urls.py
+++ DCustoMountain/mountains/urls.py
@@ -5,5 +5,8 @@ app_name = "mountains"
 urlpatterns = [
     path("filter/", views.filter, name="filter"), 
     path("<int:mountain_id>/experienced/", views.experienced, name="experienced"), 
-    path("search/", views.search_mtn_info, name="search_mtn_info")
+    path("search/", views.search_mtn_info, name="search_mtn_info"),
+    path("search/details/<int:pk>/", views.search_mtn_details, name="search_mtn_details")
+    
+
 ]
--- DCustoMountain/mountains/views.py
+++ DCustoMountain/mountains/views.py
@@ -31,4 +31,44 @@ def experienced(request, mountain_id):
     return redirect(url_next)
 
 def search_mtn_info(request):
+#    keyword = request.GET.get("keyword", None)
+#    print(keyword)
+#    print(request.GET) # request.GET으로 전달된 데이터를 출력
+    mtn_difficulty = request.GET.get("mtn_difficulty", None) # 난이도 intermediate
+    location = request.GET.get("location", None) # 지역 Seoul
+    leadtime = request.GET.get("leadtime", None) # 소요시간 3to5
+    mtn_name = request.GET.get("mtn_name", None) # 산이름 산이름오나7
+
+    # Mountain 테이블에서 모든 데이터를 가져온 후 필터링 적용
+    query = Mountain.objects.all()
+
+    # 난이도 필터
+    if mtn_difficulty and mtn_difficulty != '상관없음':
+        query = query.filter(mtn_difficulty=mtn_difficulty)
+
+    # 지역 필터
+    if location and location != '00':  # '00'은 선택 안 함
+        query = query.filter(location__icontains=location)
+
+    # 소요시간 필터
+    if leadtime and leadtime != 'no_matter':  # 'no_matter'는 선택 안 함
+        query = query.filter(leadtime=leadtime)
+
+    # 산 이름 필터
+    if mtn_name:
+        query = query.filter(mtn_name__icontains=mtn_name)
+
+    # 최종 검색 결과
+    search_result_list = query.distinct()  # 중복 제거
+
+    print('----- 검색 결과 -----')
+    print(search_result_list)
+    print(len(search_result_list))
+    print(search_result_list[0].mtn_difficulty)
+
     return render(request, "mountains/search_mtn_info.html")
+
+def search_mtn_details(request, pk):
+
+
+    return render(request, "mountains/search_mtn_details.html")
