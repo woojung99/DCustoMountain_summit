722c68af68f0fb6b6ee9fe9bde7995c2e17028a7
Jung <tr248267@gmail.com>
2025-02-26 11:42:11 +0900

    sqllite3로 DB에 등록

--- DCustoMountain/drivers/mountains/register_mtnInfo_to_db.py
+++ DCustoMountain/drivers/mountains/register_mtnInfo_to_db.py
@@ -1,23 +1,22 @@
-from mountains.models import Mountain
+import sqlite3
 
 mtn_name_list = [] # 산 이름 리스트
-
 location_name_list = [] # 지역 이름 리스트
 leadtime_list = [] # 산행 기간 리스트
 mtn_height_list = [] # 산 높이 리스트
 mtn_difficulty_list = [] # 산 난이도 리스트
 
-class register_info(mtn_name_list, location_name_list, leadtime_list, mtn_height_list, mtn_difficulty_list):
-    # Mountain모델 DB등록
-    for i in range(100):
-        # Mountain 모델의 인스턴스 생성
-        mountain = Mountain(
-            location = location_name_list[i], 
-            name = mtn_name_list[i], 
-            height = mtn_height_list[i], 
-            mtn_difficulty = mtn_difficulty_list[i], 
-            leadtime = leadtime_list[i], 
-        )
+con = sqlite3.connect(r'C:\Users\ITSC\git\DCustoMountain_summit\DCustoMountain\db.sqlite3')
 
-        # 데이터베이스에 저장
-        mountain.save()
+cur = con.cursor()
+
+sql_columns = f"""INSERT INTO mountains_mountain(location,name,height,mtn_difficulty,leadtime) values"""
+print(sql_columns)
+
+for i in range(len(mtn_name_list)):
+    cur.execute(f"""INSERT INTO mountains_mountain(location,name,height,mtn_difficulty,leadtime) values ('{location_name_list[i]}', '{mtn_name_list[i]}', '{mtn_height_list[i]}', '{mtn_difficulty_list[i]}', '{leadtime_list[i]}');""")
+    con.commit()
+
+con.close()
+
+
